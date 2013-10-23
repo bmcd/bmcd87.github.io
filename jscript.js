9ef9ac73b910aaf3425d7ddc986353ed92891756
bmcd87 <bradmcdermott@gmail.com>
2013-10-23 18:22:53 -0400

    Changes offset of autoscroll

--- jscript.js
+++ jscript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("body").on("click", "a.scroll", function(event) {
 		event.preventDefault();
 		$('html, body').animate({
-			scrollTop: $($(this).attr("href")).offset().top
+			scrollTop: $($(this).attr("href")).offset().top + 20
 		}, 200);
 	})
 })
